d3951ab1e7dffdb112f65232a5433aa1dc1f92fe
shyamravikumar6 <shyamravikumar6@gmail.com>
2021-09-23 16:21:47 +0530

    stripe-checkout-page

---
+++ README.md
@@ -0,0 +1,31 @@
+
+### Development
+1. Build the application
+```shell
+$ npm install
+```
+
+2. _Optional_: download and run the [Stripe CLI](https://stripe.com/docs/stripe-cli)
+```shell
+$ stripe listen --forward-to localhost:3000/api/webhooks
+```
+
+3. Run the application
+```shell
+$ STRIPE_WEBHOOK_SECRET=$(stripe listen --print-secret) npm run dev
+```
+
+4. Go to [localhost:3000](http://localhost:3000)
+
+### Production
+1. Build the application
+```shell
+$ npm install
+
+$ npm build
+```
+
+2. Run the application
+```shell
+$ npm start
+```
---
+++ pages/api/checkout.js
@@ -0,0 +1,47 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+      try {
+        // Create Checkout Sessions from body params.
+        // const price_data = await stripe
+        // const product = await stripe.products.create({
+        //   name: 'T-shirt',
+        // });
+       
+        // const price = await stripe.prices.create({
+        //   product: product.id,
+        //   unit_amount: req.body.price,
+        //   currency: 'usd',
+        // });
+     
+       console.log(req.body);
+
+        // const session = await stripe.checkout.sessions.create({
+        //   line_items: [
+        //     {
+        //       // TODO: replace this with the `price` of the product you want to sell
+        //       price: price.id,
+        //       quantity: 1,
+            
+        //     },
+        //   ],
+        //   payment_method_types: [
+        //     'card',
+          
+            
+        //   ],
+          
+        //   mode: 'payment',
+        //   success_url: `${req.headers.origin}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+        //   cancel_url: `${req.headers.origin}/?canceled=true`,
+        // });
+  
+        res.redirect(303, `localhost:3001/?client_secret=${1234}`);
+      } catch (err) {
+        res.status(err.statusCode || 500).json(err.message);
+      }
+    } else {
+      res.setHeader('Allow', 'POST');
+      res.status(405).end('Method Not Allowed');
+    }
+  }
